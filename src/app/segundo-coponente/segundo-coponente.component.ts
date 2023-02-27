7cf4c564ac13a1beb0849c6c04c3622ec61f6b0a
wesley <wesley.gomes222@hotmail.com>
2023-02-27 19:22:24 -0300

    Property Binding e Event Binding

--- src/app/segundo-coponente/segundo-coponente.component.ts
+++ src/app/segundo-coponente/segundo-coponente.component.ts
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 export class SegundoCoponenteComponent {
   nome = "João";
   dataNascimento = "1995-01-01";
+  urlImagem = "/assets/flamengo.webp";
+
+  mostrarDataNascimento(){
+    alert(`A data de nascimento é: ${this.dataNascimento}`)
+  }
 }

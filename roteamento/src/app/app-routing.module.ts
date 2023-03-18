3470553a5c9e9f85a614cdf2b542cad3a1502598
wesley <wesley.gomes222@hotmail.com>
2023-03-18 18:46:49 -0300

    404 - página não encontrada

--- roteamento/src/app/app-routing.module.ts
+++ roteamento/src/app/app-routing.module.ts
@@ -1,12 +1,14 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
 
 const routes: Routes = [
   { path: "primeira-pagina", component: PrimeiraPaginaComponent },
   { path: "segunda-pagina", component: SegundaPaginaComponent },
-  { path: "", redirectTo: "primeira-pagina", pathMatch: "full"}
+  { path: "", redirectTo: "primeira-pagina", pathMatch: "full"},
+  { path: "**", component: PaginaNaoEncontradaComponent }
 ]
 
 
--- roteamento/src/app/app.module.ts
+++ roteamento/src/app/app.module.ts
@@ -5,12 +5,14 @@ import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
+import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
 
 @NgModule({
   declarations: [
     AppComponent,
     PrimeiraPaginaComponent,
-    SegundaPaginaComponent
+    SegundaPaginaComponent,
+    PaginaNaoEncontradaComponent
   ],
   imports: [
     BrowserModule,

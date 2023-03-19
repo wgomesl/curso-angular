ed2b16b5ea138ca1a1be643ea4cd3eac64c2b822
wesley <wesley.gomes222@hotmail.com>
2023-03-19 00:10:11 -0300

    autenticação e proteção de rotas

--- roteamento/src/app/app-routing.module.ts
+++ roteamento/src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { AuthGuard } from './auth.guard';
+import { LoginComponent } from './login/login.component';
 import { PaginaComParametrosComponent } from './pagina-com-parametros/pagina-com-parametros.component';
 import { PaginaNaoEncontradaComponent } from './pagina-nao-encontrada/pagina-nao-encontrada.component';
+import { PaginaProtegidaComponent } from './pagina-protegida/pagina-protegida.component';
 import { PrimeiraPaginaComponent } from './primeira-pagina/primeira-pagina.component';
 import { SegundaPaginaComponent } from './segunda-pagina/segunda-pagina.component';
 
@@ -11,6 +14,8 @@ const routes: Routes = [
   { path: "", redirectTo: "primeira-pagina", pathMatch: "full"},
   { path: "pagina-com-parametros/:id", component: PaginaComParametrosComponent },
   { path: 'lazy-loading', loadChildren: () => import('./lazy-loading/lazy-loading.module').then(m => m.LazyLoadingModule) },
+  { path: "pagina-protegida", component: PaginaProtegidaComponent, canActivate: [AuthGuard] },
+  { path: "login", component: LoginComponent },
   { path: "**", component: PaginaNaoEncontradaComponent }
 ]
 
